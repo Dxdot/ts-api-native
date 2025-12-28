1ee7ba72d3df8d7fbaaf92804f01fa668799831d
Dxdot <fahm.iqbal@Outolook.co.id>
2025-12-28 19:37:24 +0700

    pembenahan db menggunakan env

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,23 @@
 import mysql from "mysql2/promise" ;
+import "dotenv/config" ;
 
-export const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "fahmi144",
-  database: "my_api_crud"
-}) ;
+const {
+  db_host,
+  db_user,
+  db_password,
+  db_name,
+  db_port
+} = process.env ;
+
+if(!db_host || !db_user || !db_name){
+  throw new Error("salah cuy") ;
+}
+
+const poolConfig = {
+  host: db_host,
+  user: db_user,
+  database: db_name,
+  ...(db_password && { password: db_password }),
+};
+
+export const db = mysql.createPool(poolConfig);
